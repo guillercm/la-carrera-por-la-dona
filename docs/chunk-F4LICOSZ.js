1e269589c1590ae5e4b501d4daf06a97149b2db5
guille3222 <guillermo.ruiz.cortina.martinez@gmail.com>
2025-04-29 22:18:27 +0200

    .

--- docs/chunk-F4LICOSZ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-QEZ3K4DX.js";var o=[{path:"login",loadComponent:()=>import("./chunk-LTDHTQQX.js")},{path:"**",redirectTo:"login"}];export{o as routes};
---
+++ docs/chunk-MSLKUBHE.js
@@ -0,0 +1,1 @@
+import"./chunk-QEZ3K4DX.js";var o=[{path:"login",loadComponent:()=>import("./chunk-5CMSYQ5C.js")},{path:"**",redirectTo:"login"}];export{o as routes};
